5a5b8f1b63c0ae94d87b7fc0d735fea5b156e6d1
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-10-13 18:24:19 -0300

    Error handling using catch operator

---
+++ src/app/app.error-handler.ts
@@ -0,0 +1,18 @@
+import {Response} from '@angular/http';
+import {Observable} from 'rxjs';
+
+export class ErrorHandler {
+
+  static handleError(error: Response | any) {
+    let errorMessage: string;
+
+    if (error instanceof Response) {
+      errorMessage = `Error ${error.status} to get URL: ${error.url} | ${error.statusText}`;
+    } else {
+      errorMessage = error.toString();
+    }
+
+    console.log(errorMessage);
+    return Observable.throw(errorMessage);
+  }
+}
